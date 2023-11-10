813d2e034e5a7ab0bd9ac1999610858ca12c47e8
enochCodes <henoksamson57@gmail.com>
2023-11-10 07:27:01 +0300

    ubdate the code of the missing number finder

---
+++ pass_betty.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * findMissingNumber - Find the missing number in an array
+ * @nums: Array of integers
+ * @n: Number of elements in the array
+ * This function finds the missing number in the array of integers from 0 to n.
+ * Return: it returns the missing number.
+ */
+int findMissingNumber(int nums[], int n)
+{
+	int expectedSum = n * (n + 1) / 2;
+	int actualSum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		actualSum += nums[i];
+	}
+	return (expectedSum - actualSum);
+}
